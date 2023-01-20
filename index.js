944a9dbb967ec2433f783cbd3a1cc70fac0e54f3
sachin972 <190113027@hbtu.ac.in>
2023-01-20 22:52:55 +0530

    modified file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+//jshint esversion:6
 const express = require('express');
+
+// import express from 'express';
 const app = express();
 const port = 80;
 
 app.get('/',function(req,res){
-    // res.sendFile(__dirname+'/index.html');
-    res.send("Hello Express");
+    res.sendFile(__dirname+'/views/index.html');
+    // res.send("Hello Express");
 });
 
-app.listen(port,()=>{
-    console.log("hello");
-});
+app.listen(port);
