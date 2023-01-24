4eddd7cd27c54edadb747f6ab7c852953a76cec6
sachin972 <190113027@hbtu.ac.in>
2023-01-24 22:32:22 +0530

    add

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+app.use(express.static(__dirname + '/static'));
+
+
+
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/views/index.html');
     // res.send("Hello Express");
 });
+
+app.get('/json',(req,res)=>{
+    var obj = {
+        'message': 'Hello Json'
+    };
+    if(process.env.MESSAGE_INFO == 'uppercase'){
+        res.send(obj.stringify().toUpperCase());
+    }
+    res.send(obj);
+})
 
 app.listen(port);
