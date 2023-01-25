750cbf389140ce53eb778a33ccf0f1cdbbd1c574
sachin972 <190113027@hbtu.ac.in>
2023-01-26 00:59:09 +0530

    adding modified files

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //jshint esversion:6
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
+// const process = require('dotenv');
 // import express from 'express';
 const app = express();
 const port = 80;
@@ -15,13 +18,41 @@ app.get('/',function(req,res){
 });
 
 app.get('/json',(req,res)=>{
-    var obj = {
-        'message': 'Hello Json'
+    console.log(req.method, req.path, req.ip);
+    res.send("Okay");
+});
+
+app.get('/now',(req,res,next)=>{
+    req.time = new Date().toString();
+    next();
+},(req,res,next)=>{
+    res.send(req.time);
+    next();
+});
+
+// var nvar = "";
+
+app.get(`/:word/echo`,(req,res,next)=>{
+
+    const obj = {
+        'echo': req.params.word
     };
-    if(process.env.MESSAGE_INFO == 'uppercase'){
-        res.send(obj.stringify().toUpperCase());
+    res.send(obj);
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/name',(req,res)=>{
+    res.send(`<form action="name" method="post"><input type="text" name="first" placeholder = "first"><input type="text" name = "second" placeholder = "second"><input type = "submit"></form>`);
+});
+
+
+app.post('/name',(req,res)=>{
+    // const q = req.body.second;
+    const obj = {
+        'name': `${req.body.first} ${req.body.second}`
     }
     res.send(obj);
-})
+});
 
 app.listen(port);
